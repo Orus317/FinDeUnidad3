70586d386447021de6437e598730736c52742c1e
Oriol Palacios <orioloriolpalacios@outlook.com>
2023-01-27 09:28:32 -0500

    Se agregó la biblioteca de estructuras de datos (arbol binario de búsqueda y el arbol AVL)

---
+++ EstructurasDeDatos/CArbolAVL.cs
@@ -0,0 +1,241 @@
+﻿namespace EstructurasDeDatos
+{
+    public class CArbolAVL : CArbolBB
+    {
+        #region ***********************  Atributos   *************************
+        private int aFE; // -- Factor de equilibrio, necesario para determinar si un nodo está o no balanceado
+        #endregion Atributos  
+
+        #region  ======================  Constructores  ======================= 
+        /* -------------------------------------------------------------- */
+        public CArbolAVL() : base()
+        {
+            aFE = 0;
+        }
+
+        /* -------------------------------------------------------------- */
+        public CArbolAVL(Object pRaiz) : base(pRaiz)
+        {
+            aFE = 0;
+        }
+
+        /* -------------------------------------------------------------- */
+        public CArbolAVL(CArbolAVL pSubArbolIzq, Object pRaiz, CArbolAVL pSubArbolDer)
+            : base(pSubArbolIzq, pRaiz, pSubArbolDer)
+        {
+            aFE = 0;
+        }
+
+
+        /* -------------------------------------------------------------- */
+        public static CArbolAVL CrearAVL()
+        {
+            return new CArbolAVL();
+        }
+
+        /* -------------------------------------------------------------- */
+        public static CArbolAVL CrearAVL(Object pRaiz)
+        {
+            return new CArbolAVL(pRaiz);
+        }
+
+        /* -------------------------------------------------------------- */
+        public static CArbolAVL CrearAVL(CArbolAVL pSubArbolIzq, Object pRaiz, CArbolAVL pSubArbolDer)
+        {
+            return new CArbolAVL(pSubArbolIzq, pRaiz, pSubArbolDer);
+        }
+        #endregion Constructores
+
+        #region *********************  Propiedades  ***********************
+        /* ---------------------------------------------------------- */
+        public int FE
+        {
+            get
+            {
+                return aFE;
+            }
+            set
+            {
+                aFE = value;
+            }
+        }
+
+        #endregion *********************  Propiedades  ***********************
+
+        #region ====================   Metodos    ====================== 
+
+        /* -------------------------------------------------------------- */
+        /*
+			private bool EstaBalanceado()
+			{
+				int Altura1 = (SubArbolIzq==null ? 0 :1+SubArbolIzq.Altura()); 
+				int Altura2 = (SubArbolDer==null ? 0 :1+SubArbolDer.Altura()); 
+				return (Math.Abs(Altura1-Altura2)<2);
+			}
+        */
+
+        /* -------------------------------------------------------------- */
+        protected void RotacionSimpleIzq(bool FlagFE = true)
+        {
+            // La rotación se efectuara primero creando un nuevo arbol y reordenando los enlaces de los arboles
+            SubArbolDer = CArbolAVL.CrearAVL(SubArbolIzq.SubArbolDer as CArbolAVL, Raiz, SubArbolDer as CArbolAVL);
+            Raiz = SubArbolIzq.Raiz;
+            SubArbolIzq = SubArbolIzq.SubArbolIzq;
+            // -- Actualizar FE
+            if (FlagFE)
+            {
+                FE = 0; // -- El nodo donde se realizaó la rotación, está balanceado
+                (SubArbolDer as CArbolAVL).FE = 0; // -- El nodo rotado también está balanceado
+            }
+        }
+
+        /* -------------------------------------------------------------- */
+        protected void RotacionDobleIzq(Object Elemento)
+        {
+            // -- Determinar condicones para actualizar FE
+            bool FlagHoja = SubArbolIzq.SubArbolDer.EsHoja();
+            bool FlagIzqDerIzq = false;
+            if (!FlagHoja)
+                FlagIzqDerIzq = (Elemento.ToString().CompareTo(SubArbolIzq.SubArbolDer.Raiz.ToString()) < 0);
+            // -- Efectuar rotaciones
+            ((CArbolAVL)SubArbolIzq).RotacionSimpleDer(false);
+            RotacionSimpleIzq(false);
+            // -- Actualizar factor de equilibrio
+            FE = 0; // -- El nodo donde se efectúa la rotación está balanceado
+            if (FlagHoja)
+                (SubArbolDer as CArbolAVL).FE = 0;
+            else if (FlagIzqDerIzq)
+            {
+                (SubArbolIzq as CArbolAVL).FE = 0;
+                (SubArbolDer as CArbolAVL).FE = 1;
+            }
+            else
+            {
+                (SubArbolIzq as CArbolAVL).FE = -1;
+                (SubArbolDer as CArbolAVL).FE = 0;
+            }
+        }
+
+        /* -------------------------------------------------------------- */
+        protected void RotacionSimpleDer(bool FlagFE = true)
+        {
+            // La rotación se efectuara primero creando un nuevo arbol y reordenando los enlaces de los arboles
+            SubArbolIzq = CArbolAVL.CrearAVL(SubArbolIzq as CArbolAVL, Raiz, SubArbolDer.SubArbolIzq as CArbolAVL);
+            Raiz = SubArbolDer.Raiz;
+            aFE = 0;
+            SubArbolDer = SubArbolDer.SubArbolDer;
+            // -- Actualizar FE
+            if (FlagFE)
+            {
+                FE = 0; // -- El nodo donde se realizaó la rotación, está balanceado
+                (SubArbolIzq as CArbolAVL).FE = 0; // -- El nodo rotado también está balanceado
+            }
+        }
+
+        /* -------------------------------------------------------------- */
+        protected void RotacionDobleDer(Object Elemento)
+        {
+            // -- Determinar condicones para actualizar FE
+            bool FlagHoja = SubArbolDer.SubArbolIzq.EsHoja();
+            bool FlagDerIzqDer = false;
+            if (!FlagHoja)
+                FlagDerIzqDer = (Elemento.ToString().CompareTo(SubArbolDer.SubArbolIzq.Raiz.ToString()) > 0);
+            // -- Efectuar rotaciones
+            ((CArbolAVL)SubArbolDer).RotacionSimpleIzq(false);
+            RotacionSimpleDer(false);
+            // -- Actualizar factor de equilibrio
+            FE = 0; // -- El nodo donde se efectúa la rotación está balanceado
+            if (FlagHoja)
+                (SubArbolIzq as CArbolAVL).FE = 0;
+            else if (FlagDerIzqDer)
+            {
+                (SubArbolIzq as CArbolAVL).FE = -1;
+                (SubArbolDer as CArbolAVL).FE = 0;
+            }
+            else
+            {
+                (SubArbolIzq as CArbolAVL).FE = 0;
+                (SubArbolDer as CArbolAVL).FE = 1;
+            }
+        }
+
+        /* -------------------------------------------------------------- */
+        public int Agregar(object Elemento)
+        {
+            int Ind = 0;
+            if (Raiz == null)
+                Raiz = Elemento;
+            else
+                if (Elemento.ToString().CompareTo(Raiz.ToString()) < 0)
+            {   // Agregar el nuevo elemento como hijo Izq
+                if (SubArbolIzq == null)
+                {
+                    SubArbolIzq = CArbolAVL.CrearAVL(null, Elemento, null);
+                    aFE--;
+                    Ind = 1;
+                }
+                else
+                {
+                    Ind = (SubArbolIzq as CArbolAVL).Agregar(Elemento);
+                    if (Ind != 0)
+                    {
+                        aFE -= Ind;
+                        // Balancear arbol si esta desbalanceado 
+                        if (aFE == -2) // Si el Factor de equilibrio es -2 entonces hay desbalance
+                        {
+                            if (Elemento.ToString().CompareTo(SubArbolIzq.Raiz.ToString()) < 0)
+                                RotacionSimpleIzq();
+                            else
+                                RotacionDobleIzq(Elemento);
+                            Ind = 0;
+                        }
+                    }
+                }
+            }
+            else
+            {   // Agregar el nuevo elemento como hijo Der
+                if (SubArbolDer == null)
+                {
+                    SubArbolDer = CArbolAVL.CrearAVL(null, Elemento, null);
+                    aFE++;
+                    Ind = 1;
+                }
+                else
+                {
+                    Ind = (SubArbolDer as CArbolAVL).Agregar(Elemento);
+                    if (Ind != 0)
+                    {
+                        aFE++;
+                        // Balancear arbol si esta desbalanceado 
+                        if (aFE == 2)
+                        {
+                            if (Elemento.ToString().CompareTo(SubArbolDer.Raiz.ToString()) > 0)
+                                RotacionSimpleDer();
+                            else
+                                RotacionDobleDer(Elemento);
+                            Ind = 0;
+                        }
+                    }
+                }
+            }
+            return Ind;
+        }
+
+        /* -------------------------------------------------------------- */
+        public void PreOrdenFE()
+        {
+            if (Raiz != null)
+            {
+                // ----- Procesar la raiz
+                Console.WriteLine(Raiz.ToString() + "- " + aFE.ToString());
+                // ----- Procesar hijo Izq 
+                if (SubArbolIzq != null)
+                    (SubArbolIzq as CArbolAVL).PreOrdenFE();
+                // ----- Procesar hijo Der 
+                if (SubArbolDer != null)
+                    (SubArbolDer as CArbolAVL).PreOrdenFE();
+            }
+        }
+        #endregion Metodos
+    }
+}
---
+++ EstructurasDeDatos/CArbolBB.cs
@@ -0,0 +1,375 @@
+﻿namespace EstructurasDeDatos
+{
+    public class CArbolBB
+    {
+        #region ***********************  Atributos   ************************* 
+        private CArbolBB aSubArbolIzq;
+        private object aRaiz;
+        private CArbolBB aSubArbolDer;
+
+        #endregion Atributos  
+
+        #region *********************  Constructores   ***********************
+
+        /* -------------------------------------------------------------- */
+        public CArbolBB()
+        {
+            aSubArbolIzq = null;
+            aRaiz = null;
+            aSubArbolDer = null;
+        }
+
+        /* -------------------------------------------------------------- */
+        public CArbolBB(object pRaiz)
+        {
+            aSubArbolIzq = null;
+            aRaiz = pRaiz;
+            aSubArbolDer = null;
+        }
+
+        /* -------------------------------------------------------------- */
+        public CArbolBB(CArbolBB pSubArbolIzq, object pRaiz, CArbolBB pSubArbolDer)
+        {
+            aSubArbolIzq = pSubArbolIzq;
+            aRaiz = pRaiz;
+            aSubArbolDer = pSubArbolDer;
+        }
+
+
+        #endregion Constructores
+
+        #region *********************  Propiedades  ***********************
+        /* ---------------------------------------------------------- */
+        public CArbolBB SubArbolIzq
+        {
+            get
+            {
+                return aSubArbolIzq;
+            }
+            set
+            {
+                aSubArbolIzq = value;
+            }
+        }
+
+        /* ---------------------------------------------------------- */
+        public object Raiz
+        {
+            get
+            {
+                return aRaiz;
+            }
+            set
+            {
+                aRaiz = value;
+            }
+        }
+
+        /* ---------------------------------------------------------- */
+        public CArbolBB SubArbolDer
+        {
+            get
+            {
+                return aSubArbolDer;
+            }
+            set
+            {
+                aSubArbolDer = value;
+            }
+        }
+
+        #endregion Propiedades
+
+        #region ***********************  Metodos  *************************
+
+        /* -------------------------------------------------------------- */
+        public bool EstaVacio()
+        {
+            return (aRaiz == null);
+        }
+
+        /* -------------------------------------------------------------- */
+        public virtual void Agregar(object Elemento)
+        {
+            if (aRaiz == null)
+            {
+                aRaiz = Elemento;
+            }
+            else
+            {
+                if (Elemento.ToString().CompareTo(aRaiz.ToString()) < 0)
+                {
+                    if (aSubArbolIzq == null)
+                        aSubArbolIzq = new CArbolBB(null, Elemento, null);
+                    else
+                        aSubArbolIzq.Agregar(Elemento);
+                }
+                else
+                {
+                    if (aSubArbolDer == null)
+                        aSubArbolDer = new CArbolBB(null, Elemento, null);
+                    else
+                        aSubArbolDer.Agregar(Elemento);
+                }
+            }
+        }
+
+        /* -------------------------------------------------------------- */
+        private void EliminarHoja()
+        {
+            aRaiz = null;
+        }
+
+        /* -------------------------------------------------------------- */
+        private void EliminarNodoConHijoIzq()
+        {
+            aRaiz = aSubArbolIzq.Raiz;
+            aSubArbolDer = aSubArbolIzq.aSubArbolDer;
+            aSubArbolIzq = aSubArbolIzq.aSubArbolIzq;
+        }
+
+        /* -------------------------------------------------------------- */
+        private void EliminarNodoConHijoDer()
+        {
+            aRaiz = aSubArbolDer.Raiz;
+            aSubArbolIzq = aSubArbolDer.SubArbolIzq;
+            aSubArbolDer = aSubArbolDer.SubArbolDer;
+        }
+
+        /* -------------------------------------------------------------- */
+        private void EliminarNodoConDosHijos()
+        {
+            Object RaizAux = aRaiz;
+            aRaiz = aSubArbolDer.Minimo();
+            aSubArbolDer.Eliminar(RaizAux);
+        }
+
+        /* -------------------------------------------------------------- */
+        private void EliminarNodo()
+        {
+            // -- Verificar si es Hoja
+            if ((aSubArbolIzq == null) && (aSubArbolDer == null))
+                EliminarHoja();
+            // -- Verificar si es nodo sólo con hijo izq
+            else if ((aSubArbolIzq != null) && (aSubArbolDer == null))
+                EliminarNodoConHijoIzq();
+            // -- Verificar si es nodo sólo con hijo der
+            else if ((aSubArbolIzq == null) && (aSubArbolDer != null))
+                EliminarNodoConHijoDer();
+            // -- Nodo tiene ambos hijos
+            else
+                EliminarNodoConDosHijos();
+        }
+
+        /* -------------------------------------------------------------- */
+        public virtual void Eliminar(Object pRaiz)
+        {
+            if (EstaVacio())
+                Console.WriteLine("ERROR. Elemento no encontrado...");
+            else
+            {
+                // ----- Verificar si la raiz es el elemento que se desea eliminar
+                if (pRaiz.Equals(aRaiz))
+                    EliminarNodo();
+                else
+                {
+                    // ----- Verificar si el elemento a eliminar esta en el hijo Izq
+                    if (pRaiz.ToString().CompareTo(aRaiz.ToString()) < 0)
+                    {
+                        if (aSubArbolIzq != null)
+                            aSubArbolIzq.Eliminar(pRaiz);
+                    }
+                    else // ----- Elemento a eliminar esta en el hijo Der
+                    {
+                        if (aSubArbolDer != null)
+                            aSubArbolDer.Eliminar(pRaiz);
+                    }
+                }
+                // Eliminar hojas vacías, verificando si los hijos son hojas vacias 
+                if ((aSubArbolIzq != null) && aSubArbolIzq.EstaVacio())
+                    aSubArbolIzq = null;
+                if ((aSubArbolDer != null) && aSubArbolDer.EstaVacio())
+                    aSubArbolDer = null;
+            }
+        }
+
+        /* -------------------------------------------------------------- */
+        public CArbolBB SubArbol(object pRaiz)
+        {
+            if (EstaVacio())
+                return null;
+            else
+                if (aRaiz.Equals(pRaiz))
+                return this;
+            else
+                    if (pRaiz.ToString().CompareTo(aRaiz.ToString()) < 0)
+                return aSubArbolIzq != null ? aSubArbolIzq.SubArbol(pRaiz) : null;
+            else
+                return aSubArbolDer != null ? aSubArbolDer.SubArbol(pRaiz) : null;
+        }
+
+        /* -------------------------------------------------------------- */
+        public CArbolBB Padre(object pRaiz)
+        {
+            if (EstaVacio())
+                return null;
+            else
+                if (EsHijo(pRaiz))
+                return this;
+            else
+                    if (pRaiz.ToString().CompareTo(aRaiz.ToString()) < 0)
+                return aSubArbolIzq != null ? aSubArbolIzq.Padre(pRaiz) : null;
+            else
+                return aSubArbolDer != null ? aSubArbolDer.Padre(pRaiz) : null;
+        }
+
+        /* -------------------------------------------------------------- */
+        public bool EsHijo(object pRaiz)
+        {
+            return (((aSubArbolIzq != null) && (aSubArbolIzq.Raiz.Equals(pRaiz))) ||
+                    ((aSubArbolDer != null) && (aSubArbolDer.Raiz.Equals(pRaiz))));
+        }
+
+        /* -------------------------------------------------------------- */
+        public bool EsHoja()
+        {
+            return (aSubArbolIzq == null) && (aSubArbolDer == null);
+        }
+
+
+        /* -------------------------------------------------------------- */
+        public object Minimo()
+        {
+            if (EstaVacio())
+                return null;
+            else
+                return SubArbolIzq == null ? aRaiz : aSubArbolIzq.Minimo();
+        }
+
+        /* -------------------------------------------------------------- */
+        public object Maximo()
+        {
+            if (EstaVacio())
+                return null;
+            else
+                return SubArbolDer == null ? aRaiz : aSubArbolDer.Maximo();
+        }
+
+        /* -------------------------------------------------------------- */
+        public int Altura()
+        {
+            if (EstaVacio())
+                return 0;
+            else
+            {
+                int AlturaIzq = (aSubArbolIzq == null ? 0 : 1 + aSubArbolIzq.Altura());
+                int AlturaDer = (aSubArbolDer == null ? 0 : 1 + aSubArbolDer.Altura());
+                return (AlturaIzq > AlturaDer ? AlturaIzq : AlturaDer);
+            }
+        }
+
+        /* -------------------------------------------------------------- */
+        public void Procesar(object Elem, Action<object> Modulo = null)
+        {
+            if (Modulo != null)
+                Modulo(Elem);
+            else
+                Console.WriteLine(Elem.ToString());
+        }
+
+        /* -------------------------------------------------------------- */
+        public void PreOrden(Action<object> Modulo = null)
+        {
+            if (aRaiz != null)
+            {
+                // ----- Procesar la raiz
+                Procesar(aRaiz, Modulo);
+                // ----- Procesar hijo Izq 
+                if (aSubArbolIzq != null)
+                    aSubArbolIzq.PreOrden(Modulo);
+                // ----- Procesar hijo Der 
+                if (aSubArbolDer != null)
+                    aSubArbolDer.PreOrden(Modulo);
+            }
+        }
+
+        /* -------------------------------------------------------------- */
+        public void InOrden(Action<object> Modulo = null)
+        {
+            if (aRaiz != null)
+            {
+                // ----- Procesar hijo Izq 
+                if (aSubArbolIzq != null)
+                    aSubArbolIzq.InOrden(Modulo);
+                // ----- Procesar la raiz
+                Procesar(aRaiz, Modulo);
+                // ----- Procesar hijo Der 
+                if (aSubArbolDer != null)
+                    aSubArbolDer.InOrden(Modulo);
+            }
+        }
+
+        /* -------------------------------------------------------------- */
+        public void PostOrden(Action<object> Modulo = null)
+        {
+            if (aRaiz != null)
+            {
+                // ----- Procesar hijo Izq 
+                if (aSubArbolIzq != null)
+                    aSubArbolIzq.PostOrden(Modulo);
+                // ----- Procesar hijo Der 
+                if (aSubArbolDer != null)
+                    aSubArbolDer.PostOrden(Modulo);
+                // ----- Procesar la raiz
+                Procesar(aRaiz, Modulo);
+            }
+        }
+
+        /* -------------------------------------------------------------- */
+        public void RecorrerCola(cCola Cola, Action<object> Modulo = null)
+        {
+            if (!Cola.EsVacia())
+            {
+                // ----- Recuperar arbol
+                CArbolBB ArbolAux = Cola.Primero() as CArbolBB;
+                Cola.Retirar();
+                // ----- Procesar raiz
+                Procesar(ArbolAux.Raiz, Modulo);
+                // ----- Agregar hijos a la cola, si existen
+                if (ArbolAux.SubArbolIzq != null)
+                    Cola.PonerEnCola(ArbolAux.SubArbolIzq);
+                if (ArbolAux.SubArbolDer != null)
+                    Cola.PonerEnCola(ArbolAux.SubArbolDer);
+                // ----- Recorrer Cola
+                RecorrerCola(Cola, Modulo);
+            }
+
+        }
+
+        /* -------------------------------------------------------------- */
+        public void RecorridoPorNiveles(Action<object> Modulo = null)
+        {
+            if (aRaiz != null)
+            {
+                // ----- Crear Cola
+                cCola Cola = new cCola();
+                // ----- Agregar Raiz a la cola
+                Cola.PonerEnCola(this);
+                // ----- Recorrer Cola
+                RecorrerCola(Cola, Modulo);
+            }
+
+        }
+
+        /* -------------------------------------------------------------- */
+        public int NroNodos()
+        {
+            if (EstaVacio())
+                return 0;
+            else
+                return 1 + (aSubArbolIzq == null ? 0 : aSubArbolIzq.NroNodos()) + (aSubArbolDer == null ? 0 : aSubArbolDer.NroNodos());
+        }
+
+        #endregion Metodos
+    }
+}
