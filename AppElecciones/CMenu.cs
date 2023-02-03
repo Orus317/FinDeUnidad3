c24c29eedef5699e18050ccbeda1438a22462f53
Ian Quispe <iancillonn@gmail.com>
2023-02-02 21:16:25 -0500

    Implementación del menú completo pero con algunas cosas a mejorar

--- AppElecciones/CMenu.cs
+++ AppElecciones/CMenu.cs
@@ -1,27 +1,65 @@
 using ClasesGenerales;
-namespace AppElecciones 
+namespace AppElecciones
 {
     public class CMenu
     {
 
-        public static void MostrarMenu(CControlPartido Partido, CControlRepresentante Representante)
+        public static void MostrarMenu(CControlPartido Partido, CControlRepresentante Representante, CArbolPartido ArbolPartido)
         {
             Console.WriteLine("========= Bibliteca =========");
             Console.WriteLine("1• Partido");
             Console.WriteLine("2• Representante");
             Console.WriteLine("3• Reportes");
-            Console.WriteLine("4• Devoluciones");
+            Console.WriteLine("4• Salir");
             Console.WriteLine();
-
             Console.Write(" -- Ingrese la opción: ");
             int Opcion = Utilidades.ValidarEntero("Debe ingresar un número del 1 al 4: ", 1, 5);
-            EjecutarOpcion(Opcion, Partido, Representante);
+            EjecutarOpcion(Opcion, Partido, Representante, ArbolPartido);
         }
 
-        private static void EjecutarOpcion(int opcion, CControlPartido partido, CControlRepresentante representante)
+        public static void MenuReportes(CControlPartido Partido, CControlRepresentante Representante, CArbolPartido ArbolPartido)
         {
-            switch (opcion)
+            Console.WriteLine("");
+            Console.WriteLine("========= Reportes =========");
+            Console.WriteLine("1. Reporte en pantalla 1");
+            Console.WriteLine("2. Reporte en pantalla 2");
+            Console.WriteLine("3. Reporte en pantalla 3");
+            Console.WriteLine("4. Reporte en pantalla 4");
+            Console.WriteLine("5. Salir ");
+            Console.WriteLine();
+            Console.Write(" -- Ingrese la opción: ");
+            int opcion;
+            do
             {
+                opcion = Utilidades.ValidarEntero("Debe ingresar un número del 1 al 5: ", 1, 5);
+                switch (opcion)
+                {
+                case 1:
+                    ArbolPartido.nroFirmasValidasMayoraN();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    ArbolPartido.PartidoPorDni();
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    CMenu.MostrarMenu(Partido, Representante, ArbolPartido);
+                    break;
+                default:
+                    break;
+                }
+            }
+            while (0 < opcion && opcion < 5);
+        }
+
+        private static void EjecutarOpcion(int opcion, CControlPartido partido, CControlRepresentante representante, CArbolPartido ArbolPartido)
+        {
+            do
+            {
+                switch (opcion)
+                {
                 case 1:
                     partido.Menu();
                     break;
@@ -29,13 +67,15 @@ namespace AppElecciones
                     representante.Menu();
                     break;
                 case 3:
-                    // Devoluciones.Ejecutar();
+                    MenuReportes(partido, representante, ArbolPartido);
                     break;
                 case 4:
                     break;
                 default:
                     break;
+                }
             }
+            while (0 < opcion && opcion < 4);
         }
     }
 }
